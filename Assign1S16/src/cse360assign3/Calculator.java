4b28da1da8fdde1161d8631e0019c7cf5c7f89c4
Arsh Padda <apadda@asu.edu>
2016-02-20 15:51:09 -0600

    Initial checkin

---
+++ Assign1S16/src/cse360assign3/Calculator.java
@@ -0,0 +1,58 @@
+
+package cse360assign3;
+/**Calculator class with no implementation 
+ * 
+ * @author Arsh Padda
+ * @version Feb 17, 2016
+ */
+public class Calculator {
+	private int total;
+	/**
+	 * Calculator class constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+	 * No implementation getTotal Method
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * No implementation add Method
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * No implementation subtract method
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * No implementation multiply method
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	/**
+	 * No implementation divide method
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	/**
+	 * No implementation getHistory method
+	 * @return Empty String
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ Assign1S16/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,48 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	/**
+	 * Check whether the memory allocation for object is done or not.
+	 */
+	public void testCalculator() {
+		Calculator obj = new Calculator();
+		assertNotNull("Object is NULL",obj);
+	}
+
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
